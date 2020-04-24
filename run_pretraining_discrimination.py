83a28301f7a8de41b5ce7c81baeea94b419c05d1
Shaoor Munir <shaoormunir@outlook.com>
2020-04-24 17:20:20 +0500

    modifying pretraining for supervised learning

--- run_pretraining_discrimination.py
+++ run_pretraining_discrimination.py
@@ -310,6 +310,28 @@ def get_next_sentence_output(bert_config, input_tensor, labels):
     loss = tf.reduce_mean(per_example_loss)
     return (loss, per_example_loss, log_probs)
 
+def get_synthetic_text_output(bert_config, input_tensor, labels):
+  """Get loss and log probs for the next sentence prediction."""
+
+  # Simple binary classification. Note that 0 is "organic" and 1 is
+  # "synthetic". This weight matrix is not used after pre-training.
+  with tf.variable_scope("cls/synthetic_discrimination"):
+    output_weights = tf.get_variable(
+        "output_weights",
+        shape=[2, bert_config.hidden_size],
+        initializer=modeling.create_initializer(bert_config.initializer_range))
+    output_bias = tf.get_variable(
+        "output_bias", shape=[2], initializer=tf.zeros_initializer())
+
+    logits = tf.matmul(input_tensor, output_weights, transpose_b=True)
+    logits = tf.nn.bias_add(logits, output_bias)
+    log_probs = tf.nn.log_softmax(logits, axis=-1)
+    labels = tf.reshape(labels, [-1])
+    one_hot_labels = tf.one_hot(labels, depth=2, dtype=tf.float32)
+    per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+    loss = tf.reduce_mean(per_example_loss)
+    return (loss, per_example_loss, log_probs)
+
 
 def gather_indexes(sequence_tensor, positions):
   """Gathers the vectors at the specific positions over a minibatch."""
